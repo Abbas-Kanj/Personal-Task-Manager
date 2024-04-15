f4c956ee333b5feb09c341beb9553b987c27b3bc
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-15 16:14:44 +0300

    basic folder structure

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Hero from "../Hero";
 
 function App() {
   return (
     <>
-      <h1 className="  text-xl font-bold text-red-500 ">Hello Tailwind</h1>
+      <Hero></Hero>
     </>
   );
 }
