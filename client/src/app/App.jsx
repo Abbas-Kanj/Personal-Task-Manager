3328be469b058360dda33b168ac692595e889f61
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-16 07:43:36 +0300

    basic styling of a single board

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "../pages/Auth/index";
 import store from "./store";
 import { Provider } from "react-redux";
+import Board from "../pages/Board";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Auth />}></Route>
+          <Route path="/" element={<Board />}></Route>
         </Routes>
       </BrowserRouter>
     </Provider>
--- client/src/pages/Board/index.jsx
+++ client/src/pages/Board/index.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import "./style.css";
 
 const Board = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="flex column center">
+      <div className="flex column center add-tasks">
+        <h3 className="bold">Add Task</h3>
+        <input type="text" name="" id="" placeholder="...." />
+      </div>
+      <div className="flex justify-evenly p container">
+        <div className="todo">
+          <h3 className="bold medium-font">Todos</h3>
+          <ul className="mg-top bg-blue">
+            <li>TODO 1</li>
+            <li>TODO 1</li>
+            <li>TODO 1</li>
+          </ul>
+        </div>
+        <div className=" in-prog">
+          <h3 className="bold medium-font">In Progress</h3>
+          <ul className="mg-top bg-blue">
+            <li>In Progress 1</li>
+            <li>In Progress 1</li>
+            <li>In Progress 1</li>
+          </ul>
+        </div>
+        <div className="done">
+          <h3 className="bold medium-font">Done</h3>
+          <ul className="mg-top bg-blue">
+            <li>Done 1</li>
+            <li>Done 1</li>
+            <li>Done 1</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
