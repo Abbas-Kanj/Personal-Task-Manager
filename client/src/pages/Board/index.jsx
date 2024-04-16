0b767106293ffe17174dc5fd3365f3f1de3908d0
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-16 08:20:04 +0300

    implementing drag and drop from dnd beautiful

--- client/src/pages/Board/index.jsx
+++ client/src/pages/Board/index.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const taskContent = [
+  {
+    id: 1,
+    description: "todo 1",
+  },
+  {
+    id: 2,
+    description: "todo 2",
+  },
+  {
+    id: 3,
+    description: "todo 3",
+  },
+];
 
 const Board = () => {
+  const [tasks, updateTasks] = useState(taskContent);
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(tasks);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateTasks(items);
+  }
   return (
     <div className="flex column center">
       <div className="flex column center add-tasks">
@@ -11,11 +37,32 @@ const Board = () => {
       <div className="flex justify-evenly p container">
         <div className="todo">
           <h3 className="bold medium-font">Todos</h3>
-          <ul className="mg-top bg-blue">
-            <li>TODO 1</li>
-            <li>TODO 1</li>
-            <li>TODO 1</li>
-          </ul>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="tasks">
+              {(provided) => (
+                <ul
+                  className="mg-top bg-blue"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {tasks.map(({ id, description }, i) => (
+                    <Draggable key={id} draggableId={String(id)} index={i}>
+                      {(provided) => (
+                        <li
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          {description}
+                        </li>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
         </div>
         <div className=" in-prog">
           <h3 className="bold medium-font">In Progress</h3>
